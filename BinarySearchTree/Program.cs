57fa0b58e333d3d5ef7e620b908bcb2c70f917a0
Ramya.U <ramyayadav2016@gmail.com>
2020-11-09 21:31:27 +0530

    [Ramya] Add . Added Node.cs and BinarySearchTree.cs

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -4,9 +4,19 @@ namespace BinarySearchTree
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Binary Search Tree!");
+            //Creating an object of bst
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.DisplayBST(bst.root);
         }
     }
 }
